47c0c684634cac0848eb15095560989e0e34e97e
Andrey Fedinyak <fedinyakandrey@gmail.com>
2022-01-29 02:32:23 +0300

    make test

--- __tests__/genDiff.test.js
+++ __tests__/genDiff.test.js
@@ -1,19 +1,18 @@
+import { test, expect } from '@jest/globals';
+import * as path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
 import genDiff from '../src/genDiff.js';
 
-const file1 = {
-  host: 'hexlet.io',
-  timeout: 50,
-  proxy: '123.234.53.22',
-  follow: false,
-};
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
-const file2 = {
-  timeout: 20,
-  verbose: true,
-  host: 'hexlet.io',
-};
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 
-const difFiles = '{  - follow: false    host: hexlet.io  - proxy: 123.234.53.22  - timeout: 50  + timeout: 20  + verbose: true}';
+const file1 = getFixturePath('file1.json');
+const file2 = getFixturePath('file2.json');
+
+const difFiles = '{\n  - follow: false,\n    host: hexlet.io,\n  - proxy: 123.234.53.22,\n  - timeout: 50,\n  + timeout: 20,\n  + verbose: true\n}';
 
 test('genDiff', () => {
   expect(genDiff(file1, file2)).toEqual(difFiles);
