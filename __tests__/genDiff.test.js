96efe117616313a5eb08a1a9adad1c0bcff76b29
Andrey Fedinyak <fedinyakandrey@gmail.com>
2022-02-01 18:13:15 +0300

    add yaml test

--- __tests__/genDiff.test.js
+++ __tests__/genDiff.test.js
@@ -9,11 +9,18 @@ const __dirname = dirname(__filename);
 
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 
-const file1 = getFixturePath('file1.json');
-const file2 = getFixturePath('file2.json');
+const JsonFile1 = getFixturePath('file1.json');
+const JsonFile2 = getFixturePath('file2.json');
+
+const YamlFile1 = getFixturePath('file1.json');
+const YamlFile2 = getFixturePath('file2.json');
 
 const difFiles = '{\n  - follow: false,\n    host: hexlet.io,\n  - proxy: 123.234.53.22,\n  - timeout: 50,\n  + timeout: 20,\n  + verbose: true\n}';
 
-test('genDiff', () => {
-  expect(genDiff(file1, file2)).toEqual(difFiles);
+test('genDiffJson', () => {
+  expect(genDiff(JsonFile1, JsonFile2)).toEqual(difFiles);
+});
+
+test('genDiffYml', () => {
+  expect(genDiff(YamlFile1, YamlFile2)).toEqual(difFiles);
 });
